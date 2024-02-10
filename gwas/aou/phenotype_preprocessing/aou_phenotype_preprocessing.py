b57f7b5f7c68f3391e5aa859b21d61f1b345a23c
gymreklab <melissa.gymrek@gmail.com>
2024-02-09 21:01:16 -0800

    updating covar

--- gwas/aou/phenotype_preprocessing/aou_phenotype_preprocessing.py
+++ gwas/aou/phenotype_preprocessing/aou_phenotype_preprocessing.py
@@ -23,11 +23,26 @@ def ERROR(msg_str):
     MSG("ERROR: " + msg_str)
     sys.exit(1)
 
+def SQLToDF(sql):
+    return pd.read_gbq(
+        sql,
+        dialect="standard",
+        use_bqstorage_api=("BIGQUERY_STORAGE_API_ENABLED" in os.environ),
+        progress_bar_type="tqdm_notebook")
+
+# TODO: if endtime < measurement time then also set to 0?
+def OverlapDrugMeasurement(x):
+	if x["measurement_datetime"] < x["drug_exposure_start_datetime"]:
+		return 0
+	else:
+		return 1
+
 def main():
     parser = argparse.ArgumentParser(__doc__)
     parser.add_argument("--phenotype", help="Phenotype ID", type=str, required=True)
     parser.add_argument("--samples", help="List of sample IDs to keep", type=str)
     parser.add_argument("--concept-id", help="Concept ID for phenotype", type=str, required=True)
+    parser.add_argument("--drugexposure-covariate-concept-ids", help="Comma-separated list of conceptid:conceptname to use as drug exposure covariates", type=str)
     parser.add_argument("--units", help="Comma-separated list of acceptable units. Accepted shorthands: blood", type=str, required=True)
     parser.add_argument("--range", help="min, max acceptable phenotype values", type=str, required=True)
 
@@ -35,24 +50,16 @@ def main():
     MSG("Processing %s"%args.phenotype)
 
     # Set up dataframes
-    demog = pd.read_gbq(
-        aou_queries.demographics_sql,
-        dialect="standard",
-        use_bqstorage_api=("BIGQUERY_STORAGE_API_ENABLED" in os.environ),
-        progress_bar_type="tqdm_notebook")
-    ptdata = pd.read_gbq(
-        aou_queries.ConstructTraitSQL(args.concept_id),
-        dialect="standard",
-        use_bqstorage_api=("BIGQUERY_STORAGE_API_ENABLED" in os.environ),
-        progress_bar_type="tqdm_notebook")
+    demog = SQLToDF(aou_queries.demographics_sql)
+    ptdata = SQLToDF(aou_queries.ConstructTraitSQL(args.concept_id))
     data = pd.merge(ptdata, demog, on="person_id", how="inner")
     MSG("After merge, have %s data points"%data.shape[0])
 
     # Restrict to samples we want to keep
     if args.samples is not None:
-    	use_samples = [int(item.strip()) for item in \
-    		open(args.samples, "r").readlines()]
-    	data = data[data["person_id"].isin(use_samples)]
+        use_samples = [int(item.strip()) for item in \
+            open(args.samples, "r").readlines()]
+        data = data[data["person_id"].isin(use_samples)]
     MSG("After filter samples, have %s data points"%data.shape[0])
 
     # Filtering
@@ -82,12 +89,30 @@ def main():
 
     # Record age info
     filtered["age"] = filtered['measurement_datetime'].dt.year - \
-    	filtered["date_of_birth"].dt.year
+        filtered["date_of_birth"].dt.year
+
+    # Optionally add any trait specific drugexposure covariates
+    covar_cols = []
+    if args.drugexposure_covariate_concept_ids is not None:
+        covar_concepts = args.drugexposure_covariate_concept_ids.strip().split(",")
+        for ci in covar_concepts:
+            concept_id, concept_name = ci.split(":")
+            drug_sql = aou_queries.ConstructDrugExposureSQL(concept_id)
+            drugdata = SQLToDF(drug_sql)
+            drugdata = drugdata.groupby(["person_id"]).agg(start=('drug_exposure_start_datetime', np.min), \
+                end=('drug_exposure_end_datetime', np.max)).reset_index()
+            drugdata = pd.merge(drugdata, filtered[["person_id", "measurement_datetime"]], on="person_id")
+            drugdata[concept_name] = drugdata.apply(OverlapDrugMeasurement, 1)
+            filtered = pd.merge(filtered, drugdata[["person_id", concept_name]], \
+                on="person_id", how="left")
+            filtered = filtered.fillna(value={"concept_name": 0})
+            MSG("After add %s, filtered has %s data points"%(concept_name, filtered.shape[0]))
+            covar_cols.append(concept_name)
 
     # Output final phenotype value
     MSG("Final file has %s data points"%filtered.shape[0])
     filtered.rename({"value_as_number": "phenotype"}, inplace=True, axis=1)
-    filtered[["person_id", "phenotype", "age"]].to_csv(args.phenotype+"_phenotypes.csv", index=False)
+    filtered[["person_id", "phenotype", "age"]+covar_cols].to_csv(args.phenotype+"_phenotypes.csv", index=False)
 
 if __name__ == "__main__":
     main()
--- gwas/aou/phenotype_preprocessing/aou_queries.py
+++ gwas/aou/phenotype_preprocessing/aou_queries.py
@@ -42,6 +42,120 @@ def GetPhenotypeRange(range):
     minval, maxval = range.strip().split(",")
     return float(minval), float(maxval)
 
+def ConstructDrugExposureSQL(concept_id):
+    return """
+    SELECT
+        d_exposure.person_id,
+        d_exposure.drug_concept_id,
+        d_standard_concept.concept_name as standard_concept_name,
+        d_standard_concept.concept_code as standard_concept_code,
+        d_standard_concept.vocabulary_id as standard_vocabulary,
+        d_exposure.drug_exposure_start_datetime,
+        d_exposure.drug_exposure_end_datetime,
+        d_exposure.verbatim_end_date,
+        d_exposure.drug_type_concept_id,
+        d_type.concept_name as drug_type_concept_name,
+        d_exposure.stop_reason,
+        d_exposure.refills,
+        d_exposure.quantity,
+        d_exposure.days_supply,
+        d_exposure.sig,
+        d_exposure.route_concept_id,
+        d_route.concept_name as route_concept_name,
+        d_exposure.lot_number,
+        d_exposure.visit_occurrence_id,
+        d_visit.concept_name as visit_occurrence_concept_name,
+        d_exposure.drug_source_value,
+        d_exposure.drug_source_concept_id,
+        d_source_concept.concept_name as source_concept_name,
+        d_source_concept.concept_code as source_concept_code,
+        d_source_concept.vocabulary_id as source_vocabulary,
+        d_exposure.route_source_value,
+        d_exposure.dose_unit_source_value 
+    FROM
+        ( SELECT
+            * 
+        FROM
+            `""" + os.environ["WORKSPACE_CDR"] + """.drug_exposure` d_exposure 
+        WHERE
+            (
+                drug_concept_id IN  (
+                    SELECT
+                        DISTINCT ca.descendant_id 
+                    FROM
+                        `""" + os.environ["WORKSPACE_CDR"] + """.cb_criteria_ancestor` ca 
+                    JOIN
+                        (
+                            select
+                                distinct c.concept_id 
+                            FROM
+                                `""" + os.environ["WORKSPACE_CDR"] + """.cb_criteria` c 
+                            JOIN
+                                (
+                                    select
+                                        cast(cr.id as string) as id 
+                                    FROM
+                                        `""" + os.environ["WORKSPACE_CDR"] + """.cb_criteria` cr 
+                                    WHERE
+                                        concept_id IN (""" + str(concept_id) + """
+                                        ) 
+                                        AND full_text LIKE '%_rank1]%'
+                                ) a 
+                                    ON (
+                                        c.path LIKE CONCAT('%.',
+                                    a.id,
+                                    '.%') 
+                                    OR c.path LIKE CONCAT('%.',
+                                    a.id) 
+                                    OR c.path LIKE CONCAT(a.id,
+                                    '.%') 
+                                    OR c.path = a.id) 
+                                WHERE
+                                    is_standard = 1 
+                                    AND is_selectable = 1
+                                ) b 
+                                    ON (
+                                        ca.ancestor_id = b.concept_id
+                                    )
+                            )
+                        )  
+                        AND (
+                            d_exposure.PERSON_ID IN (
+                                SELECT
+                                    distinct person_id  
+                            FROM
+                                `""" + os.environ["WORKSPACE_CDR"] + """.cb_search_person` cb_search_person  
+                            WHERE
+                                cb_search_person.person_id IN (
+                                    SELECT
+                                        person_id 
+                                    FROM
+                                        `""" + os.environ["WORKSPACE_CDR"] + """.cb_search_person` p 
+                                    WHERE
+                                        has_whole_genome_variant = 1 
+                                ) 
+                            )
+                    )
+                ) d_exposure 
+            LEFT JOIN
+                `""" + os.environ["WORKSPACE_CDR"] + """.concept` d_standard_concept 
+                    ON d_exposure.drug_concept_id = d_standard_concept.concept_id 
+            LEFT JOIN
+                `""" + os.environ["WORKSPACE_CDR"] + """.concept` d_type 
+                    ON d_exposure.drug_type_concept_id = d_type.concept_id 
+            LEFT JOIN
+                `""" + os.environ["WORKSPACE_CDR"] + """.concept` d_route 
+                    ON d_exposure.route_concept_id = d_route.concept_id 
+            LEFT JOIN
+                `""" + os.environ["WORKSPACE_CDR"] + """.visit_occurrence` v 
+                    ON d_exposure.visit_occurrence_id = v.visit_occurrence_id 
+            LEFT JOIN
+                `""" + os.environ["WORKSPACE_CDR"] + """.concept` d_visit 
+                    ON v.visit_concept_id = d_visit.concept_id 
+            LEFT JOIN
+                `""" + os.environ["WORKSPACE_CDR"] + """.concept` d_source_concept 
+                    ON d_exposure.drug_source_concept_id = d_source_concept.concept_id"""
+
 def ConstructTraitSQL(concept_id):
     return """
     SELECT
