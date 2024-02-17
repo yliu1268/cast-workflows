6036325fd7d4eab7e8be68836ac76839f200ae72
Melissa Gymrek <melissagymrek@Melissas-MBP.attlocal.net>
2024-02-16 20:50:04 -0800

    update preprocess

--- gwas/aou/phenotype_preprocessing/process_phenotypes.py
+++ gwas/aou/phenotype_preprocessing/process_phenotypes.py
@@ -50,7 +50,7 @@ def UploadGCS(outfile, gcsfile):
 manifest = open(ptfile.replace(".csv",".manifest.csv"), "w")
 ptdata = pd.read_csv(ptfile)
 for index, row in ptdata.iterrows():
-	sys.stderr.write("Processing {phenotype}\n".format(phenotype=row["phenotype"]))
+    sys.stderr.write("Processing {phenotype}\n".format(phenotype=row["phenotype"]))
     cmd = """./aou_phenotype_preprocessing.py \
            --phenotype {phenotype} \
            --concept-id {concept} \
