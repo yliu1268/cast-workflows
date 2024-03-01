1aae9428ee51dab3949c03da0e82b0a59d208b13
Nichole <55827494+nicholema@users.noreply.github.com>
2024-03-01 09:12:23 -0800

    Update hail_runner.py

--- gwas/aou/hail_runner.py
+++ gwas/aou/hail_runner.py
@@ -10,7 +10,7 @@ MT_WGS_PATH = 'gs://fc-aou-datasets-controlled/v7/wgs/short_read/snpindel/acaf_t
 SMALLNUM = 10e-400
 
 class HailRunner:
-    def __init__(self, ptcovar, region=None, covars=[], sample_call_rate = None, variant_call_rate = None, MAF = None, HWE = None, GQ = None):
+    def __init__(self, ptcovar, region=None, covars=[], sample_call_rate=None, variant_call_rate=None, MAF=None, HWE=None, GQ=None):
 
         self.ptcovar = ptcovar
         self.region = region
