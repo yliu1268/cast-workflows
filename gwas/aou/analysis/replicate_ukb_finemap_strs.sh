4ac2482a2716b8c4761cebd9ee300ca03085a040
Nichole Ma <nicholema@Nicholes-MBP.attlocal.net>
2024-03-02 17:22:11 -0800

    change vcf path

--- gwas/aou/analysis/replicate_ukb_finemap_strs.sh
+++ gwas/aou/analysis/replicate_ukb_finemap_strs.sh
@@ -4,7 +4,7 @@
 
 # TODO update with full UKB finemapped results
 #VCF=${WORKSPACE_BUCKET}/cromwell-execution/targetTR/4519d903-dde6-4c8e-b1ee-bcc2d7cd6dd7/call-sort_index/CBL_test.filtered.sorted.vcf.gz
-VCF=${WORKSPACE_BUCKET}/ukb_finemapped_hg38/UKB_finemapped-batch1_10.filtered.sorted.vcf.gz
+VCF=${WORKSPACE_BUCKET}/ukb_finemapped_hg38/UKB_finemapped-batch1.filtered.sorted.vcf.gz
 
 while IFS= read -r line
 do
