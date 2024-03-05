3abde4a1d55e0ac4da188be91dce50e46352830e
Melissa Gymrek <melissagymrek@Melissas-MacBook-Pro.local>
2024-03-05 11:46:09 -0800

    adding plotTR

--- gwas/aou/plotTR.py
+++ gwas/aou/plotTR.py
@@ -63,7 +63,7 @@ def main():
 
     # Merge phenotype and TR dosages
     df = pd.merge(data, trdf, on=["person_id"])
-    pltdata = df.groupby("tr_dosage").agg(phenotype_mean=("phenotype", np.mean), n=("phenotype", len))
+    pltdata = df.groupby("tr_dosage", as_index=False).agg(phenotype_mean=("phenotype", np.mean), n=("phenotype", len))
     print(pltdata.head())
 
 	# Plot - TODO
