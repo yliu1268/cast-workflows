3165fcf9dc84633697d76a52fefc44c9273394d4
nichole <nicholema621@gmail.com>
2024-04-19 09:36:21 -0700

    uncommit token

--- imputation/wdl/imputation_aou.py
+++ imputation/wdl/imputation_aou.py
@@ -44,7 +44,7 @@ def main():
 
 	args = parser.parse_args()
 
-	'''
+	
     # Get token
 	token_fetch_command = subprocess.run(['gcloud', 'auth', 'application-default', 'print-access-token'], \
 		capture_output=True, check=True, encoding='utf-8')
@@ -55,7 +55,7 @@ def main():
 	project = os.getenv("GOOGLE_PROJECT")
 	output_bucket = bucket + "/" + args.name
 
-	'''
+	
 
     # Set up workflow JSON
 	json_dict = {}
