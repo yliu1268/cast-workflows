f33d20ee7aa7ebdd067b290564da96a2fcbddcdd
Tara Mirmira <tmirmira@gmail.com>
2024-05-07 21:52:13 +0000

    add not AFR BLACK cohort

--- gwas/aou/sample_preprocessing/construct_cohorts.py
+++ gwas/aou/sample_preprocessing/construct_cohorts.py
@@ -72,12 +72,17 @@ passing_samples = passing_samples.merge(demog, on='person_id', how='inner')
 
 eur_white = passing_samples[(passing_samples['ancestry_pred']=='eur') & (passing_samples['race']=='White')]
 afr_black = passing_samples[(passing_samples['ancestry_pred']=='afr') & (passing_samples['race']=='Black or African American')]
+not_afr_black = passing_samples[!((passing_samples['ancestry_pred']=='afr') | (passing_samples['race']=='Black or African American'))]
+
 
 eur_white[['person_id', 'sex_at_birth_Male']].to_csv("EUR_WHITE.csv", index=False)
 afr_black[['person_id', 'sex_at_birth_Male']].to_csv("AFR_BLACK.csv", index=False)
+not_afr_black[['person_id', 'sex_at_birth_Male']].to_csv("NOT_AFR_BLACK.csv", index=False)
 
 
 subprocess.run("gsutil cp EUR_WHITE.csv "+bucket+"/samples/EUR_WHITE.csv", shell=True, check=True)
 subprocess.run("gsutil cp AFR_BLACK.csv "+bucket+"/samples/AFR_BLACK.csv", shell=True, check=True)
+subprocess.run("gsutil cp NOT_AFR_BLACK.csv "+bucket+"/samples/NOT_AFR_BLACK.csv", shell=True, check=True)
+
 
 
