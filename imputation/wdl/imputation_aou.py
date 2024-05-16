41af0f1ba54fbfb7a668855b4b0700c85776abf9
Nichole <55827494+nicholema@users.noreply.github.com>
2024-05-15 22:10:48 -0700

    Update imputation_aou.py
    add example code

--- imputation/wdl/imputation_aou.py
+++ imputation/wdl/imputation_aou.py
@@ -5,11 +5,12 @@ Script to launch AOU imputation use new ref panel
 example code:
 ./imputation_aou.py \
 --name 1000genome_test_imputation 
---vcf gs://genomics-public-data/1000-genomes/vcf/ALL.chr21.integrated_phase1_v3.20101123.snps_indels_svs.genotypes.vcf \
+--vcf $WORKSPACE_BUCKET/tr_imputation/tr_imputation/ALL.chr21.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz \
 --ref-panel $WORKSPACE_BUCKET/tr_imputation/tr_imputation/21_final_SNP_merged_additional_TRs.vcf.gz \
 --samples_file $WORKSPACE_BUCKET/tr_imputation/tr_imputation/test_sample.txt \
 --mem 64
 """
+
 
 import argparse
 import json
