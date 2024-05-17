60c7287473449be538f637d486c91b193e39e1d6
nichole <nicholema621@gmail.com>
2024-05-16 21:06:09 -0700

    add example code

--- imputation/wdl/imputation_aou.py
+++ imputation/wdl/imputation_aou.py
@@ -8,6 +8,7 @@ example code:
 --vcf $WORKSPACE_BUCKET/tr_imputation/tr_imputation/ALL.chr21.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz \
 --ref-panel $WORKSPACE_BUCKET/tr_imputation/tr_imputation/21_final_SNP_merged_additional_TRs.vcf.gz \
 --samples_file $WORKSPACE_BUCKET/tr_imputation/tr_imputation/test_sample.txt \
+--regions-file $WORKSPACE_BUCKET/tr_imputation/tr_imputation/sub_region.bed \
 --mem 40
 """
 
