5699ac13095152a465fab2c23fbee4ee7c378f75
nichole <nicholema621@gmail.com>
2024-05-25 12:08:30 -0700

    update example code

--- imputation/wdl/imputation_aou.py
+++ imputation/wdl/imputation_aou.py
@@ -2,14 +2,14 @@
 """
 Script to launch AOU imputation use new ref panel 
 
-example code:
+example code to impute 10 samples at CBL region 
 ./imputation_aou.py \
 --name test_imputation 
 --vcf gs://fc-aou-datasets-controlled/v7/wgs/short_read/snpindel/acaf_threshold_v7.1/vcf/acaf_threshold.chr11.vcf.bgz \
 --ref-panel $WORKSPACE_BUCKET/tr_imputation/tr_imputation/chr11_final_SNP_merged_additional_TRs.vcf.gz \
---samples_file $WORKSPACE_BUCKET/tr_imputation/tr_imputation/test_sample.txt \
+--samples-file $WORKSPACE_BUCKET/tr_imputation/tr_imputation/aou_subset_samples_10.txt \
 --regions-file $WORKSPACE_BUCKET/tr_imputation/tr_imputation/CBL_region.bed \
---mem 40
+--mem 70
 """
 
 
