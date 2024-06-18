64d1348e2333f30481605ccb7bd0c52e9136d238
nichole <nicholema621@gmail.com>
2024-06-18 10:15:06 -0700

    add ref and ref_index

--- imputation/wdl/batch_imputation_aou.py
+++ imputation/wdl/batch_imputation_aou.py
@@ -118,14 +118,14 @@ def main():
 	parser.add_argument("--mem", help="Specify run memory ", type=int, required=False, default=32)
 	parser.add_argument("--window", help="Specify window size for imputation ", type=int, required=False, default=20)
 	parser.add_argument("--samples", help="List of samples to process ", type=str, required=False, \
-					 default="bucket/tr_imputation/tr_imputation/sample/sample_manifest.txt")
+					 default="gs://fc-secure-f6524c24-64d9-446e-8643-415440f52b46/tr_imputation/tr_imputation/sample/sample_manifest.txt")
 	parser.add_argument("--region", help="Name of chrom position  chr:xxx-xxx", type=str,required=False)
 	parser.add_argument("--beagle-region", help="Apply chrom for beagle", action="store_true",required=False)
 	parser.add_argument("--subset-region", help="Subsetting region for vcf file", action="store_true",required=False)
 	parser.add_argument("--dryrun", help="Don't actually run the workflow. Just set up", action="store_true")
 	parser.add_argument("--batch-num", help="Number of batches. Default: -1 (all)",type=int, required=False, default=-1)
 	parser.add_argument("--header-file", help="Add hipstr header",type=str, required=False, \
-					 default="bucket/tr_imputation/tr_imputation/header_annotation.txt")
+					 default="gs://fc-secure-f6524c24-64d9-446e-8643-415440f52b46/tr_imputation/tr_imputation/header_annotation.txt")
 	parser.add_argument("--cromwell", help="Run using cormwell as opposed to the default cromshell",
                             action="store_true", default=False)
 
