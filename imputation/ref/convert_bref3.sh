8999873307d8bdf825a323753e4506cfac4c3234
nichole <nicholema621@gmail.com>
2024-06-24 12:52:24 -0700

    fix bug

--- imputation/ref/convert_bref3.sh
+++ imputation/ref/convert_bref3.sh
@@ -3,13 +3,18 @@
 #set up gc bucket storage"
 gcs="$WORKSPACE_BUCKET/tr_imputation/tr_imputation/ref/"
 chr=$1
-#dowaload bref3.jar
-wget https://faculty.washington.edu/browning/beagle/bref3.27May24.118.jar 
 
+#dowaload bref3.jar
+bref="bref3.27May24.118.jar"
+# Check if the file exists locally
+if [ ! -f "$bref" ]; then
+    echo "File does not exist. Downloading..."
+    wget https://faculty.washington.edu/browning/beagle/bref3.27May24.118.jar 
+else
+    echo "File already exists. Continuing..."
+fi
 
 #convert vcf to bref3
-bref="bref3.27May24.118.jar"
-
 echo "converting vcf to bref3 format"
 zcat ${chr}_final_SNP_merged_additional_TRs.vcf.gz | java -jar $bref > ${chr}_final_SNP_merged_additional_TRs.bref3
 
