59425c3f9fd993ed08d69c86b3201d8ed4af863a
nichole <nicholema621@gmail.com>
2024-06-24 12:38:12 -0700

    update

--- imputation/ref/convert_bref3.sh
+++ imputation/ref/convert_bref3.sh
@@ -12,11 +12,11 @@ bref="bref3.27May24.118.jar"
 for (( chr=1; chr<=21; chr++ ))
 do 
     echo "converting vcf to bref3 format"
-    zcat ${chr}_final_SNP_merged_additional_TRs.vcf.gz | java -jar $bref > ${chr}_final_SNP_merged_additional_TRs.bref3
+    zcat chr${chr}_final_SNP_merged_additional_TRs.vcf.gz | java -jar $bref > chr${chr}_final_SNP_merged_additional_TRs.bref3
 
     echo "Done converting"
 
-    gsutil cp ${chr}_final_SNP_merged_additional_TRs.bref3 $gcs
+    gsutil cp chr${chr}_final_SNP_merged_additional_TRs.bref3 $gcs
     echo "Successfully transfer bref to google cloud bucket"
 done 
 
