05c2c4d17b30b1aef6176b5d563f92dd115f799c
nichole <nicholema621@gmail.com>
2024-06-24 12:19:58 -0700

    modify download_vcf

--- imputation/ref/download_vcf.sh
+++ imputation/ref/download_vcf.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
 
 # Define the base URL where files are located
-vcf="https://ensemble-tr.s3.us-east-2.amazonaws.com/additional-phased-trs/chr"
+url="https://ensemble-tr.s3.us-east-2.amazonaws.com/additional-phased-trs/"
 gcs="$WORKSPACE_BUCKET/tr_imputation/tr_imputation/ref/"
-# Loop through chromosomes 1 to 21
-for (( chr=1; chr<=21; chr++ ))
-do
-    # Construct the URL for the current chromosome
-    vcf="${vcf}${chr}_final_SNP_merged_additional_TRs.vcf.gz"
-    vcf_index="${vcf}.tbi"
-    
-    # Download the file using wget
-    echo "Downloading ${vcf} ..."
-    wget "${vcf}"
-    wget "${vcf_index}"
+chr=$1
 
-    echo "Move ref vcf files to google cloud"
-    gsutil cp "chr${chr}_final_SNP_merged_additional_TRs.vcf.gz" $gcs
-done
+
+
+# Construct the URL for the current chromosome
+vcf="${chr}_final_SNP_merged_additional_TRs.vcf.gz"
+vcf_index="${chr}_final_SNP_merged_additional_TRs.vcf.gz.tbi"
+    
+# Download the file using wget
+echo "Downloading ${vcf} ..."
+wget "${url}${vcf}"
+wget "${url}${vcf_index}"
+
+echo "Move ref vcf files to google cloud"
+gsutil cp $vcf $gcs
+gsutil cp $vcf_index $gcs
 
 echo "Download complete."
 
