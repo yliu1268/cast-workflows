a2677c008dd363ad1cc3bc8c468489dbc1befb5b
nichole <nicholema621@gmail.com>
2024-06-24 16:02:26 -0700

    fix bug

--- imputation/ref/count.sh
+++ imputation/ref/count.sh
@@ -6,6 +6,7 @@ echo "chr,snp,tr" | sed 's/,/\t/g' > ${ref}
 for (( i=1; i<=22; i++ )); do
     chr="chr$i"
     vcf="${chr}_final_SNP_merged_additional_TRs.vcf.gz"
+    echo "counting ${chr}"
     count_snp=$(bcftools view -i 'ID!="."' $vcf |grep -v "^#" | wc -l)
     count_tr=$(bcftools view -i 'ID="."' $vcf |grep -v "^#" | wc -l)
 
