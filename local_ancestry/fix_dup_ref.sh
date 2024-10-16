f694fad11ae97307050ed11ce8b41b45e13a1ddc
Melissa Gymrek <melissagymrek@Melissas-MBP-2.attlocal.net>
2024-10-15 21:10:54 -0700

    add 14 to dedup list

--- local_ancestry/fix_dup_ref.sh
+++ local_ancestry/fix_dup_ref.sh
@@ -3,7 +3,7 @@
 # Fix 1kg ref panel for chroms that had
 # duplicate markers
 
-chrom=17
+chrom=14
 
 gsutil cp ${WORKSPACE_BUCKET}/gnomix/resources/ALL.chr${chrom}.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes.vcf.gz .
 
@@ -11,3 +11,6 @@ bcftools norm -d all -Oz \
 	-o ALL.chr${chrom}.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes-dedup.vcf.gz \
 	ALL.chr${chrom}.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes.vcf.gz
 tabix -p vcf ALL.chr${chrom}.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes-dedup.vcf.gz
+
+gsutil cp ALL.chr${chrom}.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes-dedup.vcf.gz ${WORKSPACE_BUCKET}/gnomix/resources/
+gsutil cp ALL.chr${chrom}.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes-dedup.vcf.gz.tbi ${WORKSPACE_BUCKET}/gnomix/resources/
