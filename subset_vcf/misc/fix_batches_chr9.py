9c77e8597ef77b951678394fff15442901f93051
Melissa Gymrek <melissagymrek@Melissas-MacBook-Pro-2.local>
2024-10-21 09:07:20 -0700

    change paths for chr9 fix batches

--- subset_vcf/misc/fix_batches_chr9.py
+++ subset_vcf/misc/fix_batches_chr9.py
@@ -9,7 +9,7 @@ json_dict = json.loads(open("chr9concat.aou.json", "r").read())
 for flist in json_dict["concatenate_batch_vcfs.batch_vcf_files"]:
 	batchnum = flist[0].split("/")[-1].split("-")[0].replace("batch","")
 	flist[-1] = os.environ["WORKSPACE_BUCKET"] + \
-		"/cromwell-execution/subset_vcf/fix_chr9/" + "batch%s-chr9-130000000-138394717.vcf.gz"%batchnum
+		"/subset_vcf/fix_chr9/" + "batch%s-chr9-130000000-138394717.vcf.gz"%batchnum
 
 with open("chr9concat.fix.aou.json", "w") as f:
 	json.dump(json_dict, f, indent=4)
